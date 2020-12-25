a7cfeb09616960bdddb495c06f0ab8cc763b89e4
phate45 <the.big.n00b@seznam.cz>
2020-12-25 20:17:13 +0100

    Formatting, main function, misc additions

--- D&D.py
+++ D&D.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Character:
 
     player = ""
@@ -11,6 +12,7 @@ class Character:
 
     def sofar(self):
         print("Your name is " + self.player + ", and your class is " + self.playclass)
+
 
 def rolldices():
     rolls = []
@@ -27,35 +29,42 @@ def rolldices():
     suma -= minroll
     return suma
 
+
 def modifier(stat):
     return (stat - 10) / 2
 
-print("Welcome to the D&D Character creator V 0.1! Let's get started, shall we?")
-print("First of, what is your name? (the player, not the character)")
-player = input(">>")
-while not player:
-    if not player:
-        print("You gotta identify yourself, y'know?")
-        player = input()
 
-c = Character(player)
+def main():
+    print("Welcome to the D&D Character creator V 0.1! Let's get started, shall we?")
+    print("First of, what is your name? (the player, not the character)")
+    player = input(">>")
+    while not player:
+        if not player:
+            print("You gotta identify yourself, y'know?")
+            player = input(">>")
 
-print("Good! Now that that's out of the way, let's go with your class. What do you want to play today?")
-print("1. Barbarian")
-playclass_verify = False
-playclass = 0
-while not playclass_verify:
-    input_playclass = input("Input number >>")
-    if not input_playclass:
-        print("Come on, it's not that hard choosing a class!")
-    else:
-        playclass = int(input_playclass)
-        if playclass > 1 or playclass < 1:
-            print("We don't have that class yet, or it doesn't exist.")
+    c = Character(player)
+
+    print("Good! Now that that's out of the way, let's go with your class. What do you want to play today?")
+    print("1. Barbarian")
+    playclass_verify = False
+    playclass = 0
+    while not playclass_verify:
+        input_playclass = input("Input number >>")
+        if not input_playclass:
+            print("Come on, it's not that hard choosing a class!")
         else:
-            playclass_verify = True
+            playclass = int(input_playclass)
+            if playclass > 1 or playclass < 1:
+                print("We don't have that class yet, or it doesn't exist.")
+            else:
+                playclass_verify = True
 
-if playclass == 1:
-    c.playclass = "Barbarian"
+    if playclass == 1:
+        c.playclass = "Barbarian"
 
-c.sofar()
+    c.sofar()
+
+
+if __name__ == "__main__":
+    main()
